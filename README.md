40c9d609d9122ccd3350108a242e4dcba24c653a
Tobias Merkl <merkl@proudsourcing.de>
2014-10-27 12:04:16 +0100

    commit 1.0.0

--- README.md
+++ README.md
@@ -1,4 +1,67 @@
-openspacer-wordpress-plugin
-===========================
+# proudsourcing-wordpress-plugin
 
-OpenSpacer API plugin for wordpress
+Wordpress plugin to get data from OpenSpacer REST API.
+
+
+## Usage
+
+	[openspacer api=events id=1 key=title]
+	[openspacer api=sessions id=1 key=title]	
+
+
+### API event data
+
+	[openspacer api=events id=EVENTID key=DATAKEY]
+	
+	Overview data keys:
+	- title
+	- url
+	- description
+	- sessionCount
+	- participantsCount
+	- sessions (list all event sessions)
+
+
+### API session data
+
+	[openspacer api=sessions id=SESSIONID key=DATAKEY]
+	
+	Overview data keys:
+	- title
+	- url
+	- abstract
+	- description
+	- ownerName
+	- ownerUrl
+	- tags
+	- likes
+	- comments
+	
+
+## Changelog
+
+	1.0.0	27.10.2014		Release
+
+
+## License
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+
+## Copyright
+
+	Proud Sourcing GmbH 2014
+	www.proudsourcing.de / openspacer.org
+	
+	
---
+++ wp-content/plugins/openspacer-api/openspacer-api.php
@@ -0,0 +1,109 @@
+<?php
+/*
+Plugin Name: OpenSpacer API
+Plugin URI: http://openspacer.org
+Description: Get data from OpenSpacer REST API
+Author: Proud Sourcing GmbH
+Author URI: http://www.proudsourcing.de
+Version: 1.0.0
+*/
+
+if( !class_exists( 'openSpacerApi' ) )
+{
+	class openSpacerApi
+	{
+		function __construct()
+		{
+			register_activation_hook( __FILE__, array( &$this, 'activate' ) );
+			register_deactivation_hook( __FILE__, array( &$this, 'deactivate' ) );
+		}
+
+		function activate()
+		{
+			// on activate
+		}
+
+		function deactivate()
+		{
+			// on deactivate
+		}
+
+		function openSpacerData( $atts )
+		{	
+			// you need to set an api key in event administration
+			$apiKey = "PUTYOURAPIKEYHERE";
+			
+			extract( shortcode_atts( array(
+				'api' => 'events',
+				'id' => null,
+				'key' => 'title'
+			), $atts ) );
+	
+			$param = "";
+			if($key == "sessions")
+			{
+				$param = "/sessions";
+			}
+			if($key == "participants")
+			{
+				$param = "/participants";
+			}
+			
+			$url = "https://openspacer.org/api/v1/secured/".$api."/".$id.$param."?apiKey=".$apiKey;
+			if($dataMethod == "fopen")
+			{
+				$json = file_get_contents($url);
+			}
+			else
+			{
+				$ch = curl_init();
+				curl_setopt($ch, CURLOPT_HEADER, 0);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+				curl_setopt($ch, CURLOPT_URL, $url);
+				$json = curl_exec($ch);
+				curl_close($ch);
+			}
+		
+			// get event session list
+			if($api == "events" && $key == "sessions" && $id != null)
+			{	
+				$data = json_decode($json);
+				$sessionList = "";
+				foreach($data as $session)
+				{
+					$sessionList .= '<li><a target="_blank" href="'.$session->url.'">'.$session->title.'</a> (<a target="_blank" href="https://openspacer.org'.$session->ownerUrl.'">'.$session->ownerName.'</a>)</li>';
+				}
+				return $sessionList;
+			}
+			
+			// get event participant list
+			if($api == "events" && $key == "participants" && $id != null)
+			{	
+				$data = json_decode($json);
+				$participantList = "";
+				foreach($data as $participant)
+				{
+					$participantList .= '<li style="list-style-type: none;"><a target="_blank" href="'.$participant->url.'"><img src="'.$participant->profilePicture.'" border="0" style="border-radius: 50%; float: left; margin-right: 15px;">'.$participant->name.'</a><br>'.$participant->city.'<br><br></li>';
+				}
+				return $participantList;
+			}
+
+			// get event data
+			if($api == "events" && $key != "sessions" && $id != null)
+			{	
+				$data = json_decode($json);
+				return $data->$key;
+			}
+			
+			// get session details
+			if($api == "sessions" && $id != null)
+			{	
+				$data = json_decode($json);
+				return $data->$key;
+			}
+
+			return;
+		}
+	}
+	add_shortcode('openspacer', array('openSpacerApi', 'openSpacerData') );
+}
